779688a79c749dd98f1e4395f1c5262129b47cc7
aditya bhojane <adityabhojane66@gmail.com>
2024-05-08 17:25:29 +0530

    learned Date types and operators

---
+++ 01_Data_types/data_type.java
@@ -0,0 +1,25 @@
+class data_type {
+    public static void main (String a[]){
+        // integer Date type 
+        byte num = 127;
+        short num2 = 32_767;
+        int num3 = 100;
+        long num4 = 100;
+
+        // for decimals
+        float decNum = 3.446f;
+        double decNum2 = 1.216321;
+
+        // char
+        String str = "adfwsa";
+        char unicode = '\u0041';
+        System.out.println(num);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(decNum);
+        System.out.println(decNum2);
+        System.out.println(str);
+        System.out.println(unicode);
+    }
+} 
